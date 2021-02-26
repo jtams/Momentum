fbd97b0462cdd9c70afca504b1e1f62186787b27
Joseph Tams <jtams322@gmail.com>
2021-02-26 15:44:10 -0800

    Fixed int

--- src/bike.js
+++ src/bike.js
@@ -56,11 +56,11 @@ module.exports = class Bike {
     speedIO(callback) {
         parser.on("data", (data) => {
             if (data.slice(0, 10) == "speedPulse") {
-                var bikeSpeed = this.frontSproketTeeth / data.split(":")[1];
+                var bikeSpeed = this.frontSproketTeeth / parseInt(data.split(":")[1]);
                 bikeSpeed *= 200;
                 bikeSpeed = bikeSpeed * (63360 / this.wheelCirc);
                 bikeSpeed = 3600000 / bikeSpeed;
-                callback(bikeSpeed);
+                callback(parseInt(bikeSpeed));
             }
         });
     }
